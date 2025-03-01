073ba6c52192cc9e66b03e441d92045f088fdfa4
Laurie Joslin <lauriejosephjoslin@gmail.com>
2025-03-01 19:16:58 +0000

    Ive had enough

--- pansharpen.py
+++ pansharpen.py
@@ -14,7 +14,7 @@ def greyscalify(image_dir: str) -> Image:
   elevation_max = np.max(lidar_elevation)
   elevation_min = np.min(lidar_elevation)
 
-  normalised_lidar = (lidar_elevation - elevation_min)/(elevation_max - elevation_min)
+  normalised_lidar = (lidar_elevation - elevation_min)/(elevation_max - elevation_min) * 100
 
   global lidar_image
 
@@ -37,20 +37,24 @@ def greyscalify(image_dir: str) -> Image:
 def pansharpen(image_array1, image_array2):
     rgb_values = np.array([0.299, 0.587, 0.114])
     psuedo_pan_array = np.true_divide((image_array1*rgb_values).sum(axis=2), rgb_values.sum())
+
     ratio = image_array2/psuedo_pan_array
     new_red = image_array1[:, :, 0] * ratio
     new_green = image_array1[:, :, 1] * ratio
     new_blue = image_array1[:, :, 2] * ratio
+
     pansharpened_array = np.stack([new_red, new_green, new_blue], axis=2)
+    pansharpened_array = np.clip(pansharpened_array, 0, 255)
+    
     pansharpened_image = Image.fromarray(pansharpened_array.astype(np.uint8))
+
     return pansharpened_image
 
 
 
 image1 = Image.open('data/satellite.jpg')
 image1 = image1.resize((5000, 5000), Image.LANCZOS)
-image1.show()
-print(image1.size)
+
 data1 = np.asarray(image1, dtype="int32")
 
 

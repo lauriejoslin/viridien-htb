5fdb9f18476ddafb255c4b48c1d19418eb7f74b8
Laurie Joslin <lauriejosephjoslin@gmail.com>
2025-03-01 17:53:16 +0000

    putting all together

--- pansharpen.py
+++ pansharpen.py
@@ -3,6 +3,41 @@ from PIL import Image
 import cv2
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import rasterio
+from skimage.transform import resize
+
+def greyscalify(image_dir: str) -> Image:
+
+  lidar = rasterio.open(image_dir)
+  lidar_elevation = lidar.read(1)
+
+  elevation_max = np.max(lidar_elevation)
+  elevation_min = np.min(lidar_elevation)
+
+  normalised_lidar = (lidar_elevation - elevation_min)/(elevation_max - elevation_min)*255
+
+  global lidar_image
+
+  lidar_image = Image.fromarray(normalised_lidar)
+
+  return lidar_image
+
+#test
+lidar_grey = greyscalify('data/DSM_TQ0075_P_12757_20230109_20230315.tif')
+lidar_grey.show()
+print(lidar_grey.size)
+
+def dimensions(path):
+   
+    image = Image.open(path)
+    width, height = image.size
+    if (width/height) == 1:
+        image = image.thumbnail((lidar_image.size))
+    else:
+        image = image.resize((lidar_image.size), Image.LANCZOS)
+    return image 
+
+
 
 
 def pansharpen(image_array1, image_array2):
@@ -17,15 +52,16 @@ def pansharpen(image_array1, image_array2):
     return pansharpened_image
 
 
-image1 = Image.open('viridien-htb/data/satellite.jpg')
+
+image1 = dimensions('data/satellite.jpg')
 image1.load()
+
 data1 = np.asarray(image1, dtype="int32")
 
-image2 = Image.open('viridien-htb/data/image_new1.jpg')
-print(image2.size)
-image2.load()
-data2 = np.asarray(image2, dtype="int32")
-print(data2.shape)
+
+data2 = np.asarray(lidar_grey, dtype="int32")
+lidar_resize = resize(data2, (500, 500))
+print(lidar_resize.shape)
 
 panimage = pansharpen(data1, data2)
 panimage.show()
